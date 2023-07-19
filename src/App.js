fd2c05d20640551f7f8a1181fc51d51bcd249af9
Harun <harunabdi8@gmail.com>
2023-07-19 17:05:36 +0200

    inital header component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/header";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body class="bg-danger">
+      <div>
+        <Header></Header>
+        <div>
+          <h2>hello</h2>
+        </div>
+      </div>
+      <div>
+        <h2>hello</h2>
+      </div>
+      <div>
+        <h2>hello</h2>
+      </div>
+    </body>
   );
 }
 
