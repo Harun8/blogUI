bc254f8715b9f72181fa8b1a622ed59b8b76a163
Harun <harunabdi8@gmail.com>
2023-09-05 16:45:12 +0200

    Impl. card footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,41 +3,16 @@ import "../css/header.css";
 
 const Footer = () => {
   return (
-    <div class="card p-color removeBorder center  ">
-      <div class="card-body ">
-        <b>
-          <p class="card-text text-info-emphasis ">Our blog</p>
-        </b>
-
-        <h1 class="card-title text-info-emphasis  display-4   ">
-          <b>
-            {" "}
-            The Untitled UI <i>journal </i>
-          </b>
-        </h1>
-
-        <b>
-          <p class="card-text text-info-emphasis my-4">
-            The Untitled UI Journal features carefully selected good works from
-            studios
-            <br /> and designers from around the globe. Subscribe for new post
-            in your inbox
-          </p>
-        </b>
-        <div class="input-group mb-3 justify-content-center w-25 mx-auto">
-          <input
-            type="text"
-            class="form-control w-25 border-2 border-success rounded "
-            placeholder="Recipient's username"
-            aria-label="Recipient's username"
-            aria-describedby="basic-addon2"
-          />
-          <div class="input-group-append">
-            <button className="btn btn-success  " type="button">
-              Get started
-            </button>
-          </div>
-        </div>
+    <div className="card text-center mx-5 bg-success py-4">
+      <div class="card-body">
+        <h5 class="card-title text-white">Start your 30-day free trial</h5>
+        <p class="card-text text-white pt-2">
+          {" "}
+          Join over 4,000+ startups already growing with Untitled
+        </p>
+        <button type="button" class="btn btn-outline-light">
+          Success
+        </button>
       </div>
     </div>
   );
